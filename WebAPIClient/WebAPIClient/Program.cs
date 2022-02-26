65e1c1a3134f16d634c84e0cd69c8c1966a0dfa7
Moris Goldshtein <Moris.Goldshtein22@myhunter.cuny.edu>
2022-02-26 15:17:17 -0500

    Activity 5

---
+++ WebAPIClient/WebAPIClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace WebAPIClient{
+    class SongLyrics{
+        [JsonProperty("lyrics")]
+        public string Lyrics{ get; set; }
+    }
+
+    class Program{
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args){
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories(){
+            while (true){
+                try{
+                    Console.WriteLine("Enter artist, enter nothing to quit");
+                    var artist = Console.ReadLine();
+                    Console.WriteLine("Enter song, enter nothing to quit");
+                    var song = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(artist) || string.IsNullOrEmpty(song)){
+                        break;
+                    }
+                    
+                    var result = await client.GetAsync("https://api.lyrics.ovh/v1/" + artist.ToLower() + "/" + song.ToLower());
+                    var resultreadable = await result.Content.ReadAsStringAsync();
+                    var songlyrics = JsonConvert.DeserializeObject<SongLyrics>(resultreadable);
+
+                    Console.WriteLine("-----------------------");
+                    Console.WriteLine(songlyrics.Lyrics);
+                    Console.WriteLine("-----------------------");
+
+                }
+                catch (Exception){
+                    Console.WriteLine("Error: Reenter valid artist and song");
+                }
+            }
+        }
+    }
+}
